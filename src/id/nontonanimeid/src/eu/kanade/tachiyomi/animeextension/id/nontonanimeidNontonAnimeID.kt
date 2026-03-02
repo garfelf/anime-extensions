c84f79f57c074c5b487322c90ff08e95e8ba4b50
garfelf <88974531+garfelf@users.noreply.github.com>
2026-03-02 19:47:59 +0800

    Create nontonanimeidNontonAnimeID.kt

---
+++ src/id/nontonanimeid/src/eu/kanade/tachiyomi/animeextension/id/nontonanimeidNontonAnimeID.kt
@@ -0,0 +1,10 @@
+package eu.kanade.tachiyomi.animeextension.id.nontonanimeid
+
+import eu.kanade.tachiyomi.animeextension.en.zorotheme.ZoroTheme
+
+class NontonAnimeID :
+    ZoroTheme(
+        "id",
+        "Nonton Anime ID",
+        "https://s11.nontonanimeid.boats",
+    )
